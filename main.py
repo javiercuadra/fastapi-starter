e362833f686aedf5199c3b10ffa1fb624a76b5df
Javier Cuadra <32556567+javiercuadra@users.noreply.github.com>
2025-11-09 18:21:31 -0500

    add new root endpoint

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {
+        "message": "Welcome! Try /greet?name=YourName to get a personalized greeting."
+    }
+
 @app.get("/greet")
 def greet(name: str = "stranger"):
     return {"message": f"Hello, {name}!"}

f4ec96916590e342feed0bc28558813a90cbd70a
Javier Cuadra <32556567+javiercuadra@users.noreply.github.com>
2025-11-10 12:07:22 -0500

    update root with better return values

--- app/routes/root.py
+++ app/routes/root.py
@@ -5,5 +5,9 @@ router = APIRouter()
 @router.get("/")
 def root():
     return {
-        "message": "Welcome! Try /greet?name=YourName to get a personalized greeting."
+        "message": "Welcome to my API. Refer below to the resources available.",
+        "resources": {
+            "greet": "/greet",
+            "math": "/math"
+        }
     }
